8228edf1216358f86e05658aeb24b11344b454c8
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-06-13 02:10:33 -0400

    Add main telemetry loop with simulated sensor updates

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from sensors import virtual_sensors    #import virtual sensors from sensors file
+from sensors import virtual_sensors   
 
 def main():
-    altitude = virtual_sensors.get_altitude()
-    temp = virtual_sensors.get_temperature()
-    velocity = virtual_sensors.get_velocity()
-
-
-    print('🚀 Rocket Telemetry Data: \n' )
-    print(f'Rocket Altitude: {altitude} m \n' )
-    print(f'Rocket Temperature: {temp} C\n' )
-    print(f'Rocket Velocity: {velocity} m/s\n' )
-
+    velocity = virtual_sensors.stimulate_velocity()
+    altitude = virtual_sensors.stimulate_altitude(velocity)
+    temp = virtual_sensors.stimulate_temperature(altitude)
 
 if __name__ == "__main__":
         main()
+
 
--- sensors/virtual_sensors.py
+++ sensors/virtual_sensors.py
@@ -1,19 +1,44 @@
-import random
+import random, time
+
+# IMPORTANT: use Lists to log the values each second
+def stimulate_velocity(): 
+    acceleration = 9.8
+    currentVelocity = 0
+    velocity_log = []
+    for t in range(30): # 30 seconds 
+        currentVelocity = currentVelocity + acceleration
+        velocity_log.append(currentVelocity)
+        print(f'Time {t+1}s - Velocity: {round(currentVelocity,2)} m/s')    
+        time.sleep(1)
+    return velocity_log
+        
+
+def stimulate_altitude(velocity):
+    currentAltitude = 0.0
+    altitude_log = []
+    for t in range(30):    #error in currentaltitude
+        currentAltitude =  currentAltitude + velocity[t] + 1  # altitude = altitude + velocity * time_interval
+        altitude_log.append(currentAltitude)
+        print(f'Time {t+1}s - Altitude: {round(currentAltitude,2)} m')  
+        time.sleep(1) 
+    return altitude_log
 
 
-# Use random to return random virtual data for the following - 
-# virtual sensors
+def stimulate_temperature(altitude):
+    # Temperature might decrease slowly as altitude goes up
+    temp = 20
+    temp_log = []
+    for t in range(30): # 30 seconds 
+        temp = temp - (altitude[t] / 10000)
+        temp_log.append(temp)
+        print(f'Time {t+1}s - Temperature: {round(temp,2)} °C')    
+        time.sleep(1)
+    return temp_log
 
-#use random.uniform() to generate values for the virtual sensors
-def get_altitude():
-    return round(random.uniform(100.0,1000.0), 2)
 
 
-def get_temperature():
-    return round(random.uniform(-50.0,150.0), 2)
 
 
-def get_velocity(): 
-    return round(random.uniform(0.0,500.0), 2)
+
 
 
